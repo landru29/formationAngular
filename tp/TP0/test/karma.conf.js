5f43aa7e111fc1015c146dfa9b00ae35d21976a1
Meichel Cyrille <cmeichel@digitaleo.com>
2014-04-30 15:59:20 +0200

    tp10

--- tp/TP0/test/karma.conf.js
+++ tp/TP0/test/karma.conf.js
@@ -1,34 +1,40 @@
-module.exports = function(config){
-    config.set({
-    basePath : '../',
+module.exports = function (config) {
+  config.set({
+    basePath: '../',
 
-    files : [
-        'js/angular/angular.js',
-        'js/angular/angular-resource.js',
-        'js/angular/angular-route.js',
-        'js/angular/angular-cookies.js',
-        'test/lib/angular/angular-mocks.js',
-        'js/angular-ui/unique.js',
-        'js/app/**/*.js',
-        'test/unit/**/*.js'
+    files: [
+      'js/angular/angular.js',
+      'js/angular/angular-resource.js',
+      'js/angular/angular-route.js',
+      'js/angular/angular-cookies.js',
+      'js/angular/angular-translate.js',
+      'js/angular/angular-translate-loader-static-files.js',
+      'js/angular/angular-translate-loader-url.js',
+      'test/lib/angular/angular-mocks.js',
+      'js/angular-ui/unique.js',
+      'js/app/**/*.js',
+      'test/unit/**/*.js'
     ],
 
-    autoWatch : true,
+    autoWatch: true,
+
+    //singleRun:true,
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers: ['Chrome'],
 
-    plugins : [
-        'karma-junit-reporter',
-        'karma-chrome-launcher',
-        'karma-firefox-launcher',
-        'karma-jasmine'       
+    plugins: [
+      'karma-junit-reporter',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
     ],
 
-    junitReporter : {
-        outputFile: 'test_out/unit.xml',
-        suite: 'unit'
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
     }
 
-})}
+  })
+}
--- tp/TP0/test/unit/controllersSpec.js
+++ tp/TP0/test/unit/controllersSpec.js
@@ -2,15 +2,50 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
+describe('controllers', function () {
+  var scope;
+  var controller;
+  var httpBackend;
+
   beforeEach(module('zenContactApp'));
 
-
-  it('should ....', inject(function() {
-    //spec body
+  beforeEach(inject(function ($rootScope, $httpBackend, $controller) {
+    scope = $rootScope.$new();
+    controller = $controller;
+    $httpBackend.expect('GET', '/rest/contacts').respond('[ { "id": 0, "lastName": "Wayne", "firstName": "Bruce", "address": "Gotham city", "phone": "555-BATMAN" },' + '{ "id": 1, "lastName": "Parker", "firstName": "Peter", "address": "New York", "phone": "555-SPDRMN" },' + '{ "id": 2, "lastName": "Storm", "firstName": "Jane", "address": "Baxter building, New York", "phone": "555-INVGRL" },' + '{ "id": 3, "lastName": "Richards", "firstName": "Red", "address": "Baxter building, New York", "phone": "555-MRFANT" },' + '{ "id": 4, "lastName": "Storm", "firstName": "Johnny", "address": "Baxter building, New York", "phone": "555-TORCH" },' + '{ "id": 5, "lastName": "Grimm", "firstName": "Benjamin", "address": "Baxter building, New York", "phone": "555-THING" },' + '{ "id": 6, "lastName": "Murdock", "firstName": "Matt", "address": "San Francisco", "phone": "555-DARDVL" },' + '{ "id": 7, "lastName": "Stark", "firstName": "Tony", "address": "Stark tower, New York", "phone": "555-IRNMAN" } ]');
+    //$httpBackend.when('GET', '/languages/en.json').respond('[]');
+    httpBackend = $httpBackend;
   }));
 
-  it('should ....', inject(function() {
+
+  it('Should not find nameFilter', function () {
+    expect(scope.nameFilter).toBeUndefined();
+  });
+
+
+  it('Should find nameFilter', function () {
+    controller('ContactListController', {
+      '$scope': scope
+    });
+    expect(scope.nameFilter).toBeDefined();
+  });
+
+  it('Should find contacts', function () {
+    controller('ContactListController', {
+      '$scope': scope
+    });
+    expect(scope.contacts).toBeDefined();
+  });
+
+  it('Should load 8 contacts', function () {
+    controller('ContactListController', {
+      '$scope': scope
+    });
+    httpBackend.flush();
+    expect(scope.contacts.length).toBe(8);
+  });
+
+  /*it('should ....', inject(function () {
     //spec body
-  }));
+  }));*/
 });
